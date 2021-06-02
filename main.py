93ad96e6613830a3023d8f0241e49d01e07bd6a5
denis <tulupovden@yandex.ru>
2021-06-02 21:28:57 +0300

    shell context

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
+from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
@@ -19,6 +20,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class Role(db.Model):

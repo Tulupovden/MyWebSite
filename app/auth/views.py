71cc12281f0108a3d0eeaa9c70680525aeed1d96
Denis Tulupov <tulupovden@yandex.ru>
2021-06-04 11:54:24 +0300

    Account confirmation

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,27 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from .. import db
+from ..email import send_email
 from ..models import User
 from .forms import LoginForm, RegistrationForm
+
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+            and not current_user.confirmed \
+            and request.endpoint \
+            and request.blueprint != 'auth' \
+            and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -38,6 +56,32 @@ def register():
                     password=form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Теперь вы можете войти в систему.')
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Подтвердите свою учетную запись',
+                   'auth/email/confirm', user=user, token=token)
+        flash('Вам было отправлено электронное письмо с подтверждением.')
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html', form=form)
+
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        db.session.commit()
+        flash('Вы подтвердили свою учетную запись. Спасибо!')
+    else:
+        flash('Ссылка для подтверждения недействительна или срок ее действия истек.')
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Потвердите ваш аккаунт',
+               'auth/email/confirm', user=current_user, token=token)
+    flash('Письмо с подтверждением было отправлено вам на электронную почту.')
+    return redirect(url_for('main.index'))

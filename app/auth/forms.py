04cb4ab5dc7d2ee669cee3b7d4026ba8e38ccd25
Denis Tulupov <tulupovden@yandex.ru>
2021-06-04 10:41:08 +0300

    registration

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,11 +1,35 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
-                                             Email()])
+    email = StringField('Электронная почта', validators=[DataRequired(), Length(1, 64),
+                                                         Email()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Оставаться в системе')
     submit = SubmitField('Войти')
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Электронная почта', validators=[DataRequired(), Length(1, 64),
+                                                         Email()])
+    username = StringField('Имя пользователя', validators=[
+        DataRequired(), Length(1, 64),
+        Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+               'Имена пользователей должны содержать только буквы, цифры, точки или '
+               'подчеркивания')])
+    password = PasswordField('Пароль', validators=[
+        DataRequired(), EqualTo('password2', message='Пароли должны совпадать.')])
+    password2 = PasswordField('Повторите пароль', validators=[DataRequired()])
+    submit = SubmitField('Зарегистрироваться')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data.lower()).first():
+            raise ValidationError('Электронная почта уже зарегистрирована.')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Такое имя пользователя уже существует.')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required
 from . import auth
+from .. import db
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -26,3 +27,17 @@ def logout():
     logout_user()
     flash('Вы вышли из системы.')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data.lower(),
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Теперь вы можете войти в систему.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
